d90a8ba4aa6ed3b5c0fb57efa474e903cfa979e5
혜미 <tomboy.kr@gmail.com>
2025-01-11 10:47:33 +0900

    Chore: 셋팅

---
+++ public/scripts/index.ts
@@ -0,0 +1,7 @@
+import React from '../lib/react.js';
+import ReactDOM from '../lib/react-dom/client.js';
+// import App from './components/app';
+
+const root = document.getElementById('app');
+
+// ReactDOM.createRoot(root).render(ReactDOM.createElement(App));
