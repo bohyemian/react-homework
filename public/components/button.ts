bac81381c78e0a9e43c84be2b34f1ad6677107b1
혜미 <tomboy.kr@gmail.com>
2025-01-12 18:12:31 +0900

    Modify: chip button aria-pressed 속성 추가
    - 피드백 반영🙂

--- public/components/button.ts
+++ public/components/button.ts
@@ -8,16 +8,21 @@ function ChipButton({ children, active }) {
     {
       type: 'button',
       className,
+      ['aria-pressed']: !!active,
       onClick: (e: MouseEvent) => {
         const target = e.currentTarget as HTMLButtonElement;
         const buttonWrap = target.closest('.button-wrap') as HTMLDivElement;
         const buttonAll = buttonWrap?.querySelectorAll('.chip-button');
 
         if (buttonAll) {
-          [...buttonAll].map((button) => button.classList.remove('is-active'));
+          [...buttonAll].forEach((button) => {
+            button.classList.remove('is-active');
+            button.setAttribute('aria-pressed', 'false');
+          });
         }
 
         target.classList.toggle('is-active');
+        target.setAttribute('aria-pressed', 'true');
       },
     },
     children
