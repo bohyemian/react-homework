e4cfa12f5e1f0464ccc2df590876875674dbba88
혜미 <tomboy.kr@gmail.com>
2025-02-05 19:06:49 +0900

    Chore: 패키지 설치
    - tailwindcss
    - clsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,39 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-const viteConfig = defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-    }),
-  ],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
-  preview: {
-    host: 'localhost',
-    port: 8080,
-  },
-  css: {
-    devSourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+const viteConfig = defineConfig((env) => {
+  return {
+    plugins: [
+      react({
+        jsxRuntime: 'automatic',
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      host: 'localhost',
+      port: 3000,
     },
-  },
+    preview: {
+      host: 'localhost',
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        localsConvention: 'camelCase',
+        generateScopedName: env.mode.includes('development')
+          ? '_[local]_[hash:base64:3]'
+          : '_[hash:base64:6]',
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 });
 
 export default viteConfig;
