c0acb813a3b5deac0cb6adb2d7728b9107283365
혜미 <tomboy.kr@gmail.com>
2025-02-08 12:14:41 +0900

    Design: 재고 마크업 위치 수정

--- src/components/cart/product-item.tsx
+++ src/components/cart/product-item.tsx
@@ -14,14 +14,14 @@ function ProductItem({name, price, stock, imgPath, index, updateQuantity}: Omit<
   const priceComma = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center relative">
       <figure className="flex items-center gap-[18px]">
         {imgPath ? <img className="w-[110px]" src={import.meta.env.BASE_URL + imgPath} alt={name} /> : null}
         <figcaption className="relative overflow-hidden text-ellipsis">
           <strong className="overflow-hidden text-ellipsis text-[34px] font-normal whitespace-nowrap">{name}</strong>
           <span className="block text-[30px] font-extrabold">{priceComma}원</span>
-          <span className="absolute inset-x-[100%] top-0 ml-3 px-2 py-1 text-[13px] whitespace-nowrap">재고: {stock}</span>
         </figcaption>
+        <span className="absolute right-0 bottom-0 min-w-[110px] text-[18px] text-(--color-gray-500) text-center whitespace-nowrap">재고: {stock}</span>
       </figure>
       <Quantity idx={index} max={stock} updateQuantity={updateQuantity} />
     </div>
