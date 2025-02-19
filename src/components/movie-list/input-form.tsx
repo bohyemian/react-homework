0619a4847ea29146fff206906a1df06d6f27169d
bohyemian <tomboy.kr@gmail.com>
2025-02-20 00:36:05 +0900

    Modify: 검색 입력 폼 css

--- src/components/movie-list/input-form.tsx
+++ src/components/movie-list/input-form.tsx
@@ -30,7 +30,7 @@ function SearchForm({setQuery}: SearchFormProps) {
         "transition-[border]")
     }>
       <label htmlFor="searchMovie" className="sr-only">영화 검색</label>
-      <input type="search" id="searchMovie" name="query" className="flex-1 px-4 text-[28px] font-bold" />
+      <input type="search" id="searchMovie" name="query" className="w-full flex-1 px-4 text-[28px] font-bold" />
       <button type="submit" aria-label="검색" title="검색" className="p-2">
         <svg width={48} height={48} aria-hidden="true" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
           <path d="M19 11.5a7.5 7.5 0 1 1-15 0 7.5 7.5 0 0 1 15 0m-2.107 5.42 3.08 3.08" />
